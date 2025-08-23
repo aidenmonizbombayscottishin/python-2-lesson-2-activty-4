71e8cf790f48a3cae258e0b5babc898df5b6a92c
aidenmonizbombayscottishin <aiden.moniz@bombayscottish.in>
2025-08-23 20:49:39 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+setc = {"green", "blue"}
+setc2 = {"blue", "yellow"}
+print("Original sets:")
+print(setc1)
+print(setc2)
+setc = setc1.union(setc2)
+print("\nUnion of the above sets:")
+print(setc)
